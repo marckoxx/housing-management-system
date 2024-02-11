1969568b0bf8bf2a93274c1b0eb6b4041aea5912
marckoxx <8marcko8@gmail.com>
2024-02-11 23:01:34 +0800

    Initial Commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Test" />
+
+    <div>Hello world.</div>
+</template>
+
+<style>
+
+</style>
